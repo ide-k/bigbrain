b6e9e0f337bc479287141d41e25a72befae4936a
sidd <siddharthkhanna2k@gmail.com>
2020-02-07 17:37:00 -0500

    doing some cpp tutorials
    putting random junk here
    variables.cpp is me using local and global variables

---
+++ c++/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// these are still comments
+/*
+ * so are these !
+ * 
+*/
+int main() {
+
+    cout<<"hi"<<endl;
+}
---
+++ c++/variables.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int g;
+
+int main() {
+
+    int tuna = 6;
+    //cout<<tuna<<endl;
+
+    double salmon = 9.8;
+
+    bool isSalmonGood = true;
+
+    // declaring
+    int a;
+    int b;
+
+    // initializing
+    a = 1;
+    b = 12;
+    g = a+b;
+
+    cout<<g<<endl;
+
+    return 0;
+}
